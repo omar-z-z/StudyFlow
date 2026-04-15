a099c6ebd1c029c8fe2969681f90fed854b78420
Omar Zuhri <omar.z.zuhri@gmail.com>
2026-04-15 17:23:06 +0300

    Fix: configure Sanctum correctly as the default authentication guard, and fixing the routing to login issue.

--- backend/config/auth.php
+++ backend/config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => env('AUTH_GUARD', 'web'),
+        'guard' => env('AUTH_GUARD', 'sanctum'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
 
@@ -36,8 +36,8 @@ return [
     */
 
     'guards' => [
-        'web' => [
-            'driver' => 'session',
+        'sanctum' => [
+            'driver' => 'sanctum',
             'provider' => 'users',
         ],
     ],
