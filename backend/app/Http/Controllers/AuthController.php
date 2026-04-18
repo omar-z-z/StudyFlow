d822b6da71fe9ffb47d36777d777b4cbe07c63a9
Omar Zuhri <omar.z.zuhri@gmail.com>
2026-04-18 15:52:36 +0300

    Fix: update namespace in UserResource and authorize method in UpdateCourseRequest

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\Auth\LoginRequest;
 use App\Http\Requests\Auth\RegisterRequest;
-use App\Http\Resources\Auth\UserResource;
+use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
